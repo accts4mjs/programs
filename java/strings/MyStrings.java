ba58580991918be00a41347933d09dd58adff5cf
Mike S <accts4mjs@gmail.com>
2017-02-16 17:24:50 -0700

    Added fizbuzz function.

--- java/strings/MyStrings.java
+++ java/strings/MyStrings.java
@@ -1,15 +1,15 @@
 import java.io.*;
 
 public class MyStrings {
-  public static final int NUM_FUNCS = 1;
+  public static final int NUM_FUNCS = 2;
   private int function_number;
   private String function_data;
-  private ErrorMsgs debugout;
+  private ErrorMsgs dbg;
 
   public MyStrings(int num, String data) {
     function_number = num;
     function_data = data;
-    debugout = new ErrorMsgs();
+    dbg = new ErrorMsgs();
   }
   public static void usage(int exit_value) {
     System.out.printf("usage: MyStrings <1-%d> <data_for_function>\n", NUM_FUNCS);
@@ -21,7 +21,7 @@ public class MyStrings {
     for (int i=function_data.length()-1; i>=0; i--) {
       reversed_string += function_data.substring(i,i+1);
     }
-    debugout.printf(String.format("rvrs: '%s'\n", reversed_string));
+    dbg.printf(String.format("rvrs: '%s'\n", reversed_string));
     if (function_data.compareTo(reversed_string) == 0) {
       System.out.println("TRUE");
       return 0;
@@ -30,10 +30,31 @@ public class MyStrings {
       return -1;
     }
   }
+  public int fizBuzz() {
+    int max = Integer.parseInt(function_data);
+
+    dbg.printf(String.format("fb: '%02d'\n", max));
+    if (max > 30) {
+      max = 30;
+    }
+    for (int i=1; i<=max; i++) {
+      dbg.printf(String.format("fb: i: %02d\n", i));
+      if ((i % 5) == 0 && (i % 3) == 0) {
+        System.out.printf("%02d: fizbuzz\n", i);
+      } else if ((i % 3) == 0) {
+        System.out.printf("%02d: fiz\n", i);
+      } else if ((i % 5) == 0) {
+        System.out.printf("%02d: buzz\n", i);
+      }
+    }
+    return 0;
+  }
   public int runFunction() {
     switch (function_number) {
       case 1:
         return checkPalindrome();
+      case 2:
+        return fizBuzz();
       default:
         System.out.print("ERROR: Unknown function # ");
         System.out.println(Integer.toString(function_number));
